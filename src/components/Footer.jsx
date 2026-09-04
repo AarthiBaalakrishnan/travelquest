a62384a8b4f75c8a925a780a9d2908f185af3ef8
Arthi <arthibalakrishnan@gmail.com>
2026-09-04 14:49:48 -0400

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../styles/Footer.css";
+
+function Footer() {
+    return(
+        <footer className="footer">
+            <p>@Travel Quest</p>
+            <p>Explore.Quiz.Save</p>
+        </footer>
+    );
+}
+
+export default Footer;
