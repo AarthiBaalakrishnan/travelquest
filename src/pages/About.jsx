753958b9a08001ce09ca2a03bd3ea1636dd37941
Arthi <arthibalakrishnan@gmail.com>
2026-09-04 14:55:06 -0400

    Add About page

---
+++ src/pages/About.jsx
@@ -0,0 +1,48 @@
+function About() {
+    return(
+        <main>
+            <section>
+                <h1>About Travel Quest</h1>
+                <p>
+                    Discover new places, explore exciting destinations,
+                    and challenge yourself with fun travel quizzes!
+                </p>
+            </section>
+            <section>
+                <h2>What Can You Do Here?</h2>
+                <div>
+                    <h3>Explore Cities</h3>
+                    <p>
+                        Browse different cities and discover famous attractions and places to visit.
+                    </p>
+                </div>
+                <div>
+                    <h3>Take Quiz</h3>
+                    <p>
+                        Test your knowledge about different travel destinations with fun quizzes.
+                    </p>
+                </div>
+                <div>
+                    <h3>Save places to Bucket List</h3>
+                    <p>
+                        Add places you want to visit to your personal travel bucket list.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <h2>Our Goal</h2>
+                <p>
+                    Our goal is to make travel planning more interactive and enjoyable. 
+                    TravelQuest combines exploration, learning, and goal-setting in one simple application.
+                </p>
+            </section>
+            <section>
+                <h2>Start your Travel Quest</h2>
+                <p>
+                    Explore. Learn. Challenge Yourself. Discover your next adventure.
+                </p>
+            </section>
+        </main>
+    );
+}
+export default About;
